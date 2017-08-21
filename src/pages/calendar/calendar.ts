3f86b6981e3cb19cdc5c0cfd88f1cb9ec3f6130c
blue_lim <chinapoopoo@gmail.com>
2017-08-22 01:43:30 +0900

    달력 바인딩 좀

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpService } from '../../services/http.service';
 
-/**
- * Generated class for the CalendarPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-calendar',
