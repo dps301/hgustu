033169e5f0f57c3596f3fb340fc0e75f4b6fb0de
blue_lim <chinapoopoo@gmail.com>
2017-08-24 00:09:29 +0900

    확인 ㄱ

--- src/pages/notice-detail/notice-detail.ts
+++ src/pages/notice-detail/notice-detail.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the NoticeDetailPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-notice-detail',
   templateUrl: 'notice-detail.html',
 })
 export class NoticeDetailPage {
-  title
-  content
-  regdate
+  title: any;
+  content: any;
+  regdate: any;
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.title = this.navParams.get('title');
     this.content = this.navParams.get('content');
