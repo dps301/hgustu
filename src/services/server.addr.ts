8147d109498d47f7a9a68999edcab67ac04bdfd5
genesis <genesis041835@gmail.com>
2017-09-03 17:15:47 +0900

    a

--- src/services/server.addr.ts
+++ src/services/server.addr.ts
@@ -1,5 +1,5 @@
 export class ServerAddr {
-  private static serverAddr = 'http://api.bluelab.me:3200';
+  private static serverAddr = 'http://stu.cafe24app.com';
   // private static serverAddr = 'http://localhost:3200';
 
   public static getServerAddr() {
